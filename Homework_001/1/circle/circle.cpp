b323c8593d2cda492d099abbb5668ec1179d41d5
ERIC1CN <111236481+ERIC1CN@users.noreply.github.com>
2022-09-29 19:05:34 +0800

    Add files via upload

---
+++ Homework_001/1/circle/circle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double a = 0, area = 0, length = 0;
+
+	a = 5;
+	area = a * a;
+	length = 4 * a;
+	cout << "正方形的面积" << area << endl;
+	cout << "正方形的周长" << length << endl;
+
+	return 0;
+
+}
---
+++ Homework_001/2/cout_1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+#define PI 3.1415926
+int main()
+{
+    cout<<"首战告捷"<< endl;
+    cout<<"Yes!!!"<< endl;
+    return 0;
+}
